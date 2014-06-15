4711cfcc235585ccf7b71cbdfaa81da5479c753a
john <onesoma@gmail.com>
2014-06-15 10:20:51 +0100

    check if file exists, check local size against remote before downloading. fixes #5, fixes #6

--- src/run.py
+++ src/run.py
@@ -41,6 +41,17 @@ def format_filename(s):
   valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
   filename = ''.join(c for c in s if c in valid_chars)
   return filename
+
+def downloadFile(req, directory, local_name):
+  dl = 0
+  total_length = req.headers.get('content-length')
+  with open(directory+'/'+local_name, 'wb') as fd:
+    for chunk in req.iter_content(chunk_size=1024):
+      dl += len(chunk)
+      fd.write(chunk)
+      done = int(50 * dl / int(total_length))
+      sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
+      sys.stdout.flush
   
   
   
@@ -88,37 +99,25 @@ for key in pairs:
   i += 1
   location = download_prefix + key
   local_name = format_filename(pairs[key][0]) + '.mp3'
+  local_length = pairs[key][3]
   directory = pairs[key][1].partition('.')[0]
   if not os.path.exists(directory):
     os.makedirs(directory)
-  print i, ': ', directory+'/'+local_name, '\n'
+  print i, ': ', directory+'/'+local_name, '\t\t'
   r = requests.get(location, stream=True)
-  total_length = r.headers.get('content-length')
-  
-  if pairs[key][2]:
-#    print '\nlocal size: ' , pairs[key][3] , '\n  remote size: ' , total_length
-    if pairs[key][3] < total_length:
-      dl = 0
-      with open(directory+'/'+local_name, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=1024):
-          dl += len(chunk)
-          fd.write(chunk)
-          done = int(50 * dl / int(total_length))
-          sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
-          sys.stdout.flush
-    else:
-      print 'Already done: ', directory+'/'+local_name
+  remote_length = int(r.headers.get('content-length'))
+  print 'pairs[key][2]:' , pairs[key][2], '\tlocal size: ' , local_length , '\tremote size: ' , remote_length
+  if not pairs[key][2]:
+    print 'Not down already, downloading'
+    downloadFile(r, directory, local_name)
+  elif (local_length < remote_length):
+#    print '(local_length < remote_length): ', (local_length < remote_length)
+    print 'Local size less than remote size, downloading'
+    downloadFile(r, directory, local_name)
   else:
-    dl = 0
-    with open(directory+'/'+local_name, 'wb') as fd:
-      for chunk in r.iter_content(chunk_size=1024):
-        dl += len(chunk)
-        fd.write(chunk)
-        done = int(50 * dl / int(total_length))
-        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
-        sys.stdout.flush
+    print 'Already done, local size: ' , pairs[key][3] , '\t  remote size: ' , remote_length
 
-  print '\n', i, ' done, ', len(pairs)-i, ' to go'
+  print i, ' done, ', len(pairs)-i, ' to go\n'
 
 
 '''
